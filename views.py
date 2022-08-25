5ff438634bc9617898daae39e8fb62e75808bf82
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-08-25 21:03:48 +0200

    adding to csv

--- views.py
+++ views.py
@@ -1,26 +1,11 @@
 import pandas as pd
-from io import StringIO
+from datetime import datetime
+
+df = pd.read_csv(r'/home/kamil/workplace/Tritem/car_details.csv', delim_whitespace=True)
 
 
-# speed in km / hour; distance in km
-data = '''checkpoint    initial-time id  speed distance speed2 distance2
-1   2020-09-18T12:03:14.485952Z car_01 72 9 71 9
-2   2020-09-18T12:10:14.485952Z car_01 83 8 82 8
-3   2020-09-18T12:15:14.485952Z car_01 73 9 73 9
-4   2020-09-18T12:25:14.485952Z car_01 74 9 74 9
-5   2020-09-18T12:35:14.485952Z car_01 72 9 72 9
-6   2020-09-18T12:50:14.485952Z car_01 72 9 71 9
-7   2020-09-18T12:59:14.485952Z car_01 72 9 72 9
-8   2020-09-18T13:05:14.485952Z car_01 80 9 80 9
-9   2020-09-18T13:14:14.485952Z car_01 71 9 71 9
-10   2020-09-18T13:19:14.485952Z car_01 70 9 71 9
-'''
-
-
-df = pd.read_csv(StringIO(data), delim_whitespace=True)
 df['elapsed-time'] = df['distance'] / df['speed'] # in hours
 df['elapsed-time2'] = df['distance2'] / df['speed2']
-
 df["initial-time"] = pd.to_datetime(df["initial-time"])
 
 
@@ -37,6 +22,17 @@ ave_speed2 = ((df.groupby('id')['distance2'].sum() / df.groupby('id')['elapsed-t
              .rename('ave speed2 (km/hr)').round(2))
 distance2 = (df.groupby('id')['distance2'].sum())
 
+new_data = {
+    "initial-time" : "2022-08-25 20:27:43.225624",
+    "id" : "car_01",
+    "speed" : "20",
+    "distance" : "5",
+    "speed2" : "20",
+    "distance2" : "5"
+}
+
+df = pd.DataFrame(list(new_data))
+df.to_csv('car_details.csv', mode='a', index=False, header=False)
 
 result = pd.concat([start_time, end_time, time, ave_speed, distance, ave_speed2, distance2], axis=1)
 
